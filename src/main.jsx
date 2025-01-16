11cc171c47e6016f0cce5c7908d401274cf5e3af
Felipe de Freitas Borges <felipedefreitasborges@gmail.com>
2025-01-15 22:26:23 -0300

    Configure React Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './assets/components/Navbar/Navbar';
 import Home from './assets/pages/Home/Home.Jsx';
 import Footer from './assets/components/Footer/Footer.Jsx';
 import Signup from './assets/pages/Signup/Signup';
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
     <Navbar />
-    <Home />
-    <Footer />
-  </React.StrictMode>
-)
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Signup' element={<Signup/>}/>
+      </Routes>
+      <Footer />
+  </BrowserRouter>
+);
