920b4fe8afe04ca9802444c8072b8a86db296dc7
Felipe de Freitas Borges <felipedefreitasborges@gmail.com>
2025-01-18 19:52:50 -0300

    Link to buttons and configurate temp of the rocket

--- src/assets/components/LogoFrequency/LogoFrequency.jsx
+++ src/assets/components/LogoFrequency/LogoFrequency.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import '../LogoFrequency/LogoFrequency.scss'
 
 
 const LogoFrequency = ({sizefont = "1.3rem"}) => {
+
     return(
-        <div className="LogoFrequency" style={{ fontSize: sizefont }}>
-            <p>FREQUENCY</p>
-        </div>
+            <Link to="/" className="LogoFrequency" style={{fontSize: sizefont}}>FREQUENCY</Link>
     );
 };
 
--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import {useNavigate} from 'react-router-dom'
 import './Navbar.scss';
 import Button from "../Variables/Button/Button";
 import LogoFrequency from "../LogoFrequency/LogoFrequency";
 
 const Navbar = () => {
+    
+    const navigate = useNavigate();
+    const goToLogin = () => {
+        navigate('/Login')
+    };
+
     return(
         <nav>
             <div><LogoFrequency /></div>
-            <div><Button label={"Entrar"}/></div>
+            <div><Button onClick={goToLogin} label={"Entrar"}/></div>
         </nav>
     )
 }
